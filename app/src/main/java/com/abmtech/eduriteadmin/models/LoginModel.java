b4a85d6aff51c08e7552b1ba41b0bf7a38d54207
ABMTechnologyOfficial <abmtech2022@gmail.com>
2023-09-27 10:26:26 +0530

    Adding Pages And Apis

---
+++ app/src/main/java/com/abmtech/eduriteadmin/models/LoginModel.java
@@ -0,0 +1,134 @@
+package com.abmtech.eduriteadmin.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class LoginModel {
+
+    @SerializedName("result")
+    @Expose
+    private String result;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+    @SerializedName("data")
+    @Expose
+    private Data data;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public class Data {
+
+        @SerializedName("id")
+        @Expose
+        private String id;
+        @SerializedName("name")
+        @Expose
+        private String name;
+        @SerializedName("username")
+        @Expose
+        private String username;
+        @SerializedName("password")
+        @Expose
+        private String password;
+        @SerializedName("fcm")
+        @Expose
+        private String fcm;
+        @SerializedName("is_prime")
+        @Expose
+        private String isPrime;
+        @SerializedName("profile_img")
+        @Expose
+        private String profileImg;
+        @SerializedName("type")
+        @Expose
+        private String type;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getFcm() {
+            return fcm;
+        }
+
+        public void setFcm(String fcm) {
+            this.fcm = fcm;
+        }
+
+        public String getIsPrime() {
+            return isPrime;
+        }
+
+        public void setIsPrime(String isPrime) {
+            this.isPrime = isPrime;
+        }
+
+        public String getProfileImg() {
+            return profileImg;
+        }
+
+        public void setProfileImg(String profileImg) {
+            this.profileImg = profileImg;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/abmtech/eduriteadmin/utils/ProgressDialog.java
@@ -0,0 +1,34 @@
+package com.abmtech.eduriteadmin.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.annotation.NonNull;
+
+import com.abmtech.eduriteadmin.R;
+
+public class ProgressDialog extends Dialog {
+    final Context context;
+
+    public ProgressDialog(@NonNull Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getAttributes().windowAnimations = R.style.animation;
+        getWindow().setDimAmount(0.7f);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.custom_progress_dialog);
+        setCanceledOnTouchOutside(false);
+        setCancelable(false);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+}
